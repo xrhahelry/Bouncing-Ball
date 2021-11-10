d377962b5ff094bcd1b0245971ba14f91bcf4e3a
SujalBajra <sujalbajracharya1@gmail.com>
2021-11-10 16:43:56 +0545

    ball changed and added a trigger to stop the program with a end message

--- ball.c
+++ ball.c
@@ -2,28 +2,38 @@
 #include<conio.h>
 #include<stdbool.h>
 
-void circle(void)
+void ball(void)
 {
-  printf("\t\t\t\t     @@@@@@@@@\n");
-  printf("\t\t\t\t   @@@@@@@@@@@@@\n");
-  printf("\t\t\t\t  @@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t @@@@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t @@@@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t@@@@@@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t@@@@@@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t @@@@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t @@@@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t  @@@@@@@@@@@@@@@\n");
-  printf("\t\t\t\t   @@@@@@@@@@@@@\n");
-  printf("\t\t\t\t     @@@@@@@@@\n");
+printf("              .......                 \n");
+printf("        .zd$$$$$$$$$$$$$bc.           \n");        
+printf("      z$$$$$$$$$$$$$$$$$$$$$c         \n");
+printf("    d$$$$$$$$$$$$$$$$$$$$$$$$$b       \n");
+printf("   z$$$$$$$$$$$$$$$$$$$$$$$******c    \n");
+printf("  J$$$$$$$$$$$$$$$**""            b   \n");
+printf(" J$$$$$$$$$$$*""                   b  \n");
+printf(".$$$$$$$$*                          L \n");
+printf("$$$$$$*       z$P**$b               3 \n");
+printf("$$$*          $$   4$F              ^r\n");
+printf("$                  d$               eF\n");
+printf(".                 d$             .d$$F\n");
+printf("L                J$\%.zeL      .e$$$$$\n");
+printf("'               4$$$P      .e$$$$$$$P \n");
+printf(" \%                     .e$$$$$$$$$$$ \n");
+printf("  \%               .ze$$$$$$$$$$$$$$  \n");
+printf("   %    ....ee$$$$$$$$$$$$$$$$$$$P    \n");
+printf("    ^$$$$$$$$$$$$$$$$$$$$$$$$$$$      \n");
+printf("      ^*$$$$$$$$$$$$$$$$$$$$$P        \n");
+printf("          *$$$$$$$$$$$$$$$*           \n");
+printf("             ^ ******                 ");       
 }
 
 
-int main(void) 
+void main(void) 
 {
   bool run = true;
   int count = 0;
   bool down = true;
+  int cycle = 0;
 
   while(run)
   {
@@ -34,17 +44,26 @@ int main(void)
       printf("\n");
     }
 
-    circle();
+    ball();
+
     if(down){
       count++;
     } else {
       count--;
     }
 
-    if (count > 15){
+    if (count > 10){
       down = false;
+      cycle++;
     } else if (count < 0) {
       down = true;
     }
+
+    if (cycle > 2) {
+      run = false;
+    }
   }
+  sleep(2);
+  system("cls");
+  printf("\n\t\tBALL HAS FINISHED BOUNCING.");
 }
